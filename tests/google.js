ceec0dcaa2471b76db422f839301eedd867eb99a
Hasan Hawar <hasan.hawar>
2021-01-14 01:12:11 +0100

    fix: correct the selector

--- tests/google.js
+++ tests/google.js
@@ -9,7 +9,7 @@ module.exports = {
       .navigate()
       .setQuery(mainQuery)
       .selectFilter("@languageDropdown", "lang_ar")
-      .selectFilter("@lastUpdateDropDown", "m")
+      .selectFilter("@lastUpdateDropDown", "w")
       .search();
 
     browser.assert
